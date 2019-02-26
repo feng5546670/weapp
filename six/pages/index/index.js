a880bef4454f073d96cc2f7ea9785eb9bd4ed88a
yopofeng <yopofeng@tencent.com>
2019-02-26 16:10:09 +0800

    test

---
+++ six/pages/index/index.js
@@ -0,0 +1,74 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'https://hartfeng-d1e6c9.tcb.qcloud.la/0e634e975705a6326a8ae7fc833760c7.jpg?sign=6415af020d02187f6cb4de67ada4f81a&t=1551167389',
+      'https://hartfeng-d1e6c9.tcb.qcloud.la/0e634e975705a6326a8ae7fc833760c7.jpg?sign=6415af020d02187f6cb4de67ada4f81a&t=1551167389',
+      'https://hartfeng-d1e6c9.tcb.qcloud.la/0e634e975705a6326a8ae7fc833760c7.jpg?sign=6415af020d02187f6cb4de67ada4f81a&t=1551167389'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
